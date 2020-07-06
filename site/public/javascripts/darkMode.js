ccff6331f0f2a529f4f06ffc04a0fa661d823a39
gersilva96 <gersilva96@hotmail.com>
2020-07-06 00:57:19 -0300

    Deleted old header js and css

--- site/public/javascripts/darkMode.js
+++
@@ -1,8 +0,0 @@
-function darkMode() {
-    var element = document.body;
-    if(element.classList.toggle("dark-mode")){
-        document.getElementById('logoHeader').src=('/images/logo/logoWhiteH.svg');
-    }else{
-        document.getElementById('logoHeader').src=('/images/logo/logoColorH.svg');
-    };
-}
