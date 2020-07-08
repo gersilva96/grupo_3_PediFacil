16fad6fea3651392ed0249590155cdb29b2dba92
gersilva96 <gersilva96@hotmail.com>
2020-07-08 12:12:49 -0300

    Actualizo función formatPrice()

--- site/src/controllers/mainController.js
+++ site/src/controllers/mainController.js
@@ -1,7 +1,18 @@
 const productsController = require("./productsController");
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-const formatPrice = (price,discount) => toThousand((price*(1-(discount/100))).toFixed(2));
+const formatPrice = (price,discount) => {
+    let priceDot;
+    if (discount == undefined) {
+        priceDot = toThousand(price.toFixed(2));
+    } else {
+        priceDot = toThousand((price*(1-(discount/100))).toFixed(2));
+    }
+    let first = priceDot.slice(0,-3);
+    let last = priceDot.slice(-3);
+    let lastReplaced = last.replace(".", ",");
+    return `$ ${first}${lastReplaced}`;
+};
 
 let mainController = {
     home: (req,res) => {    //GET - Muestro todos los productos en oferta
