380acbd198828d74438a427b140a585d437e009f
German <gersilva96@hotmail.com>
2020-07-25 17:55:59 -0300

    Añado middlewares de validacion de direcciones y metodos del carrito

--- site/src/routes/cart.js
+++ site/src/routes/cart.js
@@ -6,6 +6,10 @@ const loggedUser = require("../middlewares/loggedUser");
 const isBuyer = require("../middlewares/isBuyer");
 const addToCart = require("../middlewares/addToCart");
 const addToCartQuantity = require("../middlewares/addToCartQuantity");
+const selectAddress = require("../middlewares/selectAddress");
+const addressValidation = require("../middlewares/addressValidation");
+const processExistingAddress = require("../middlewares/processExistingAddress");
+const processNewAddress = require("../middlewares/processNewAddress");
 
 const cartController = require("../controllers/cartController");
 
@@ -18,5 +22,13 @@ router.put("/add/:id", loggedUser, isBuyer, addToCart, addToCartQuantity, cartCo
 router.delete("/delete/:id", loggedUser, isBuyer, cartController.delete);        //DELETE - Elimina un producto del carrito - Debe haber un usuario logueado - Debe tener rol de comprador
 
 router.delete("/clean", loggedUser, isBuyer, cartController.clean);        //DELETE - Vacía el carrito - Debe haber un usuario logueado - Debe tener rol de comprador
+
+router.get("/select-address", loggedUser, isBuyer, selectAddress, cartController.select);       //GET - Muestra las direcciones usadas anteriormente y un botón para agregar una nueva - Debe haber un usuario logueado - Debe tener rol de comprador - Debe tener al menos una dirección cargada con anterioridad
+
+router.get("/add-address", loggedUser, isBuyer, cartController.address);        //GET - Muestra el formulario de agregar dirección para la compra - Debe haber un usuario logueado - Debe tener rol de comprador
+
+router.post("/purchase-wn", loggedUser, isBuyer, addressValidation.new, processNewAddress, cartController.purchase);        //POST - Ejecuta la compra con una dirección nueva - Debe haber un usuario logueado - Debe tener rol de comprador
+
+router.post("/purchase-we", loggedUser, isBuyer, addressValidation.existing, processExistingAddress, cartController.purchase);     //POST - Ejecuta la compra con una dirección ya existente - Debe haber un usuario logueado - Debe tener rol de comprador
 
 module.exports = router;
