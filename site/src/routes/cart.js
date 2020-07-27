044825b9c4cf81f581f9090b46222d84728eff43
German <gersilva96@hotmail.com>
2020-07-26 21:11:54 -0300

    Actualizo las rutas del historial de compras de los usuarios

--- site/src/routes/cart.js
+++ site/src/routes/cart.js
@@ -8,8 +8,8 @@ const addToCart = require("../middlewares/addToCart");
 const addToCartQuantity = require("../middlewares/addToCartQuantity");
 const selectAddress = require("../middlewares/selectAddress");
 const addressValidation = require("../middlewares/addressValidation");
-const processExistingAddress = require("../middlewares/processExistingAddress");
-const processNewAddress = require("../middlewares/processNewAddress");
+const addressInSession = require("../middlewares/addressInSession");
+const orderDescriptionValidation = require("../middlewares/orderDescriptionValidation");
 
 const cartController = require("../controllers/cartController");
 
@@ -27,8 +27,14 @@ router.get("/select-address", loggedUser, isBuyer, selectAddress, cartController
 
 router.get("/add-address", loggedUser, isBuyer, cartController.address);        //GET - Muestra el formulario de agregar dirección para la compra - Debe haber un usuario logueado - Debe tener rol de comprador
 
-router.post("/purchase-wn", loggedUser, isBuyer, addressValidation.new, processNewAddress, cartController.purchase);        //POST - Ejecuta la compra con una dirección nueva - Debe haber un usuario logueado - Debe tener rol de comprador
+router.post("/add-new-address", loggedUser, isBuyer, addressValidation.new, cartController.newAddress);        //POST - Guarda una nueva dirección en session - Debe haber un usuario logueado - Debe tener rol de comprador
 
-router.post("/purchase-we", loggedUser, isBuyer, addressValidation.existing, processExistingAddress, cartController.purchase);     //POST - Ejecuta la compra con una dirección ya existente - Debe haber un usuario logueado - Debe tener rol de comprador
+router.post("/add-existing-address", loggedUser, isBuyer, addressValidation.existing, cartController.existingAddress);     //POST - Guarda una dirección existente en session - Debe haber un usuario logueado - Debe tener rol de comprador
+
+router.get("/finally", loggedUser, isBuyer, addressInSession, cartController.finally);      //GET  - Muestra el resumen de compra antes de finalizarla - Debe haber un usuario logueado - Debe tener rol de administrador - Debe haber una dirección en session
+
+router.post("/purchase", loggedUser, isBuyer, addressInSession, orderDescriptionValidation, cartController.purchase);       //POST - Ejecuta la compra - Debe haber un usuario logueado - Debe tener rol de comprador - Debe haber una dirección en session
+
+router.post("/repeat-purchase/:id", loggedUser, isBuyer, cartController.repeatPurchase);    //POST - Repite una compra anterior si hay stock suficiente - Debe haber un usuario logueado - Debe tener rol de comprador
 
 module.exports = router;
